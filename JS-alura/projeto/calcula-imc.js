b38e40e76a89edd9ef8b4d3f2469910c9d4d93dd
NikolaiCinotti <agencianiko@hotmail.com>
2016-09-01 18:53:13 -0300

    abstraindo pacientes

--- JS-alura/projeto/calcula-imc.js
+++ JS-alura/projeto/calcula-imc.js
@@ -1,34 +1,31 @@
 // peso / altura * altura
 
-var tdPeso = document.getElementById("peso-1");
-var tdAltura = document.getElementById("altura-1");
 
-
-var paciente1 = {
-    "peso1": tdPeso.textContent,
-    "altura1": tdAltura.textContent
-};
-
-var tdPeso2 = document.getElementById("peso-2");
-var tdAltura2 = document.getElementById("altura-2");
-
-
-var paciente2 = {
-    "peso2": tdPeso.textContent,
-    "altura2": tdAltura.textContent
-};
-var pacientesTotal = [paciente1, paciente2];
+var trsPacientes = document.getElementsByClassName("paciente");
 
 var posicaoAtual = 0;
-while (posicaoAtual <= pacientesTotal.length - 1) {
+while (posicaoAtual <= trsPacientes.length - 1) {
 
-    var pacientesTotal = pacientesTotal[posicaoAtual]
+    var pacienteTr = trsPacientes[posicaoAtual];
+
+    var tdNome = pacienteTr.getElementsByClassName("info-nome")[0];
+    var tdPeso = pacienteTr.getElementsByClassName("info-peso")[0];
+    var tdAltura = pacienteTr.getElementsByClassName("info-altura")[0];
+    var tdImc = pacienteTr.getElementsByClassName("info-imc")[0];
+
+    var paciente = {
+        "nome": tdNome.textContent,
+        "peso": tdPeso.textContent,
+        "altura": tdAltura.textContent
+    };
+
 
     if (paciente.altura != 0) {
         var imc = paciente.peso / (paciente.altura * paciente.altura);
 
-        //var tdImc = document.getElementById("imc-2");
-        //tdImc.textContent = imc;
+        var tdImc = pacienteTr.getElementsByClassName("info-imc")[0];
+        tdImc.textContent = imc;
+
         console.log(imc);
     } else {
         console.log("não executei");
