6c6e9e5b39fb861d575df7a3e3bed75039272d3d
NikolaiCinotti <agencianiko@hotmail.com>
2016-10-02 05:26:27 -0300

    Conceito de PseudoMétodos - Construtora e Fabrica
    Diferença entre eles

---
+++ conceitos/FuncaoConstrutora.js
@@ -0,0 +1,7 @@
+var Pessoa = function (nome, idade) {
+	this.nome = nome,
+	this.idade = idade;	
+};
+
+console.log(new Pessoa("Pedro", 20));
+console.log(new Pessoa("Maria", 30));
---
+++ conceitos/FuncaoFabrica.js
@@ -0,0 +1,9 @@
+var criarPessoa = function (nome, idade) {
+	return {
+		nome: nome,
+		idade: idade
+	};
+};
+
+console.log(criarPessoa("Pedro", 20));
+console.log(criarPessoa("Maria", 30));
