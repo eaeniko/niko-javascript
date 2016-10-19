11a1d1c8a53144432fd775a115708fef48cd8984
NikolaiCinotti <agencianiko@hotmail.com>
2016-10-19 21:22:34 -0200

    Concat arrays

---
+++ conceitos/API-ARRAY/API-Array-concat.js
@@ -0,0 +1,6 @@
+var carros = ["Ka", "Corsa", "Palio"];
+var motos = ["Honda", "Yamaha"];
+
+var veiculos = carros.concat(motos);
+
+veiculos.toString(); // ["Ka", "Corsa", "Palio", "Honda", "Yamaha"]
