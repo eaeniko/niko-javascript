56ac53ccfb9c4bd5b1b5ecb6651f046ca3d1b62c
Nikolai Cinotti <agencianiko@hotmail.com>
2017-04-11 12:44:15 -0300

    Validando todos os pacientes com FOR

--- JS-alura/introducao/projeto-aparecidaNutricao/js/principal.js
+++ JS-alura/introducao/projeto-aparecidaNutricao/js/principal.js
@@ -1,37 +1,45 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+for(var i = 0; i < pacientes.length; i++){
+	console.log(pacientes[i]);
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+	var paciente = pacientes[i];
 
-console.log(paciente);
-console.log(peso);
-console.log(altura);
+	var tdPeso = paciente.querySelector(".info-peso");
+	var peso = tdPeso.textContent;
 
-var tdImc = paciente.querySelector(".info-imc");
 
-var pesoValido = true;
-var alturaValida = true; 
+	var tdAltura = paciente.querySelector(".info-altura");
+	var altura = tdAltura.textContent;
 
-if(peso <= 0 || peso >= 1000) {
-	console.log("Peso Inválido");
-	pesoValido = false;
-	tdImc.textContent = "Peso inválido";
+	console.log(paciente);
+	console.log(peso);
+	console.log(altura);
+
+	var tdImc = paciente.querySelector(".info-imc");
+
+	var pesoValido = true;
+	var alturaValida = true; 
+
+	if(peso <= 0 || peso >= 1000) {
+		console.log("Peso Inválido");
+		pesoValido = false;
+		tdImc.textContent = "Peso inválido";
+	}
+
+	if(altura <= 0 || altura >= 3.00) {
+		console.log("Altura inválida!");
+		alturaValida = false;
+		tdImc.textContent = "Altura inválida";
+	}
+
+	if(alturaValida && pesoValido) {
+		var imc = peso / (altura * altura);
+		console.log(imc);
+		tdImc.textContent = imc.toFixed(2);
+	}
 }
 
-if(altura <= 0 || altura >= 3.00) {
-	console.log("Altura inválida!");
-	alturaValida = false;
-	tdImc.textContent = "Altura inválida";
-}
-
-if(alturaValida && pesoValido) {
-	var imc = peso / (altura * altura);
-	console.log(imc);
-	tdImc.textContent = imc;
-}
