aeebac8be4cf0d2c968264dd466797f8de9cf05a
Nikolai Cinotti <agencianiko@hotmail.com>
2017-04-15 16:39:44 -0300

    event.preventDefault

--- JS-alura/introducao/projeto-aparecidaNutricao/js/principal.js
+++ JS-alura/introducao/projeto-aparecidaNutricao/js/principal.js
@@ -41,8 +41,9 @@ for(var i = 0; i < pacientes.length; i++){
 }
 
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click", function() {
-	console.log("Botão clicado");
+botaoAdicionar.addEventListener("click", function(event) {
+	event.preventDefault();
+	
 });
 
 
