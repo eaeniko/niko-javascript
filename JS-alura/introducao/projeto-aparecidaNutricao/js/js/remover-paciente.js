7de61fee672f772e09abc58bb48e91a7fa7d76f0
Nikolai Cinotti <agencianiko@hotmail.com>
2017-05-10 13:10:29 -0300

    Event Bubbling

--- JS-alura/introducao/projeto-aparecidaNutricao/js/js/remover-paciente.js
+++ JS-alura/introducao/projeto-aparecidaNutricao/js/js/remover-paciente.js
@@ -1,8 +1,21 @@
 var pacientes = document.querySelectorAll(".paciente");
 
-pacientes.forEach(function(paciente)){
-	paciente.addEventListener("dblclick", function(){
-		this.remove();
-	});
+var tabela = document.querySelector("table");
 
-};
+tabela.addEventListener("dblclick", function(event){
+	var alvoEvento = event.target;
+	var paiDoAlvo = alvoEvento.parentNode; // tr = paciente
+
+	paiDoAlvo.remove(); // event.target.parentNode.remove();
+
+});
+
+
+
+
+// pacientes.forEach(function(paciente)){
+// 	paciente.addEventListener("dblclick", function(){
+// 		this.remove();
+// 	});
+
+// };
