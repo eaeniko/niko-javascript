285b09751766a939951dd606e062fc16de2d8ecd
Nikolai Cinotti <agencianiko@hotmail.com>
2017-05-11 13:56:44 -0300

    Animação na remoção

--- JS-alura/introducao/projeto-aparecidaNutricao/js/remover-paciente.js
+++ JS-alura/introducao/projeto-aparecidaNutricao/js/remover-paciente.js
@@ -3,10 +3,12 @@ var pacientes = document.querySelectorAll(".paciente");
 var tabela = document.querySelector("table");
 
 tabela.addEventListener("dblclick", function(event){
-	var alvoEvento = event.target;
-	var paiDoAlvo = alvoEvento.parentNode; // tr = paciente
+	event.target.parentNode.classList.add("fadeOut");
 
-	paiDoAlvo.remove(); // event.target.parentNode.remove();
+	setTimeout(function(){
+		event.target.parentNode.remove();
+
+	}, 1000);
 
 });
 
