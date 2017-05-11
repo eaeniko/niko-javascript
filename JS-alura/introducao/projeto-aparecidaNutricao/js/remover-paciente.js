75d665673222acde80969ca5d54f01c4b2956a32
Nikolai Cinotti <agencianiko@hotmail.com>
2017-05-11 14:02:03 -0300

    Movendo form para o inicio da pagina

--- JS-alura/introducao/projeto-aparecidaNutricao/js/remover-paciente.js
+++ JS-alura/introducao/projeto-aparecidaNutricao/js/remover-paciente.js
@@ -3,12 +3,16 @@ var pacientes = document.querySelectorAll(".paciente");
 var tabela = document.querySelector("table");
 
 tabela.addEventListener("dblclick", function(event){
-	event.target.parentNode.classList.add("fadeOut");
 
-	setTimeout(function(){
-		event.target.parentNode.remove();
+	if(event.target.tagName == 'TD'){
+		
+		event.target.parentNode.classList.add("fadeOut");
 
-	}, 1000);
+		setTimeout(function(){
+			event.target.parentNode.remove();
+
+		}, 1000);
+	}
 
 });
 
