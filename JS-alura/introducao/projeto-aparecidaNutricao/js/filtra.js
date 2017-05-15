2d98d891db03edbb6ad6d387ecb47db7dbd1bde2
Nikolai Cinotti <agencianiko@hotmail.com>
2017-05-15 19:10:23 -0300

    expressão regular

--- JS-alura/introducao/projeto-aparecidaNutricao/js/filtra.js
+++ JS-alura/introducao/projeto-aparecidaNutricao/js/filtra.js
@@ -2,10 +2,26 @@ var campoFiltro = document.querySelector("#filtrar-tabela");
 
 campoFiltro.addEventListener("input", function(){
 	var pacientes = document.querySelectorAll(".paciente");
-	for(var i = 0; i < pacientes.length ; i++){
-		var paciente = pacientes[i];
-		var tdNome = paciente.querySelector(".info-nome");
-		var nome = paciente.textContent;
-	}
+	
+	 if(this.value.length > 0){
+        for(var i = 0; i < pacientes.length ; i++){
 
+            var paciente = pacientes[i];
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+            var expressao = new RegExp(this.value, "i");
+
+
+            if(!expressao.test(nome)){
+                paciente.classList.add("invisivel");
+            }else{
+                paciente.classList.remove("invisivel");
+            }
+        }
+
+	}else{
+		pacientes.forEach(function(paciente){
+			paciente.classList.remove("invisivel");
+		});
+	}
 });
